d0426e3c5651dc98a421a21d6e0ab0c05ad98103
tashfiq333 <tashfiqnahiyan333@gmail.com>
2021-03-04 06:07:04 +0600

    DatabaseConnection

--- src/controller/DashBoard.java
+++ src/controller/DashBoard.java
@@ -1,9 +1,25 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.ConnectMSSQL;
+import sample.Transport;
 
-public class DashBoard {
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class DashBoard  implements Initializable {
 
     @FXML
     private Button driver_id;
@@ -13,5 +29,95 @@ public class DashBoard {
 
     @FXML
     private Button transport_id;
+
+    @FXML
+    private TableView<Transport> transport_tableView;
+
+    @FXML
+    private TableColumn<Transport, String> table_Transport_Plate_No;
+
+    @FXML
+    private TableColumn<Transport, String> table_ClassName;
+
+    @FXML
+    private TableColumn<Transport, String> table_GarageID;
+
+    @FXML
+    private TableColumn<Transport, String> table_Insurance;
+
+    @FXML
+    private TableColumn<Transport, String> table_Servicing_Cost;
+
+    @FXML
+    private TableColumn<Transport, String> table_Servicing_Query;
+
+    @FXML
+    private TableColumn<Transport, Integer> table_Capacity;
+
+    @FXML
+    private TableColumn<Transport, Integer> table_TransportCondition;
+
+
+    private String query;
+
+    // private DatabaseHandler databaseHandler;
+    private ObservableList<Transport> TransportObservableList;
+    int index = -1;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        transport_id.setOnAction(actionEvent ->{
+
+
+            TransportObservableList = FXCollections.observableArrayList();
+            initTable();
+            try {
+                loadFromDatabase();
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
+
+            transport_tableView.setItems(TransportObservableList);
+
+
+        });
+
+
+
+    }
+    private void initTable() {
+        table_Transport_Plate_No.setCellValueFactory(new PropertyValueFactory<>("transportPlateNo"));
+        table_ClassName.setCellValueFactory(new PropertyValueFactory<>("className"));
+        table_GarageID.setCellValueFactory(new PropertyValueFactory<>("garageId"));
+        table_Insurance.setCellValueFactory(new PropertyValueFactory<>("insurance"));
+        table_Servicing_Cost.setCellValueFactory(new PropertyValueFactory<>("servicingCost"));
+        table_Servicing_Query.setCellValueFactory(new PropertyValueFactory<>("servicingQuery"));
+        table_Capacity.setCellValueFactory(new PropertyValueFactory<>("capacity"));
+        table_TransportCondition.setCellValueFactory(new PropertyValueFactory<>("transportCondition"));
+    }
+
+    private void loadFromDatabase() throws SQLException, ClassNotFoundException {
+       ConnectMSSQL Database = new ConnectMSSQL();
+        Statement statement = Database.connectDB().createStatement();
+        query = "SELECT * FROM Transport";
+        ResultSet rs = statement.executeQuery(query);
+
+        while(rs.next()) {
+           System.out.println(rs.getString("class_name"));
+            System.out.println(rs.getString("Transport_plate_no"));
+            System.out.println(rs.getString("Garage_id"));
+            System.out.println(rs.getString("Insurance"));
+            System.out.println(rs.getString("Servicing_cost"));
+            System.out.println(rs.getString("Servicing_query"));
+            System.out.println(rs.getString("Capacity"));
+            System.out.println(rs.getString("Transport_condition"));
+           TransportObservableList.add(new Transport(rs.getString("Transport_plate_no"), rs.getString("class_name"),
+                   rs.getInt("Garage_id"), rs.getBoolean("Insurance"), rs.getInt("Servicing_cost"),
+                    rs.getBoolean("Servicing_query"), rs.getInt("Capacity"),rs.getString("Transport_condition")) {
+           });
+        }
+    }
 
 }
--- src/sample/ConnectMSSQL.java
+++ src/sample/ConnectMSSQL.java
@@ -1,41 +1,41 @@
 package sample;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class ConnectMSSQL {
 
     public Connection connection;
 
-    public void connectDB() {
+    public Connection connectDB() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(
                     "jdbc:sqlserver://localhost:1433;databaseName=TransportManagement;selectMethod=cursor", "sa", "123456");
 
-            System.out.println("DATABASE NAME IS:" + connection.getMetaData().getDatabaseProductName());
+//            System.out.println("DATABASE NAME IS:" + connection.getMetaData().getDatabaseProductName());
 
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement
-                    .executeQuery("SELECT Transport_plate_no FROM Transport");
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement
+//                    .executeQuery("SELECT Transport_plate_no FROM Transport");
 
 
-            while (resultSet.next()) {
-
-                System.out.println("Transport_plate_no:" +
-                        resultSet.getString("Transport_plate_no"));
-
-            }
+//            while (resultSet.next()) {
+//
+//                System.out.println("Transport_plate_no:" +
+//                        resultSet.getString("Transport_plate_no"));
+//
+//            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return connection;
     }
 
-    public static void main(String[] args) {
-        ConnectMSSQL cnObj = new ConnectMSSQL();
-        cnObj.connectDB();
-    }
+//    public static void main(String[] args) {
+//        ConnectMSSQL cnObj = new ConnectMSSQL();
+//        cnObj.connectDB();
+//    }
 
 }
